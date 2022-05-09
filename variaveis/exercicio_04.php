35bb8e3896577cb4acf308b938f15a33fa7f6631
Lucas Ribeiro <sunlight.lucas2@gmail.com>
2022-05-09 15:44:24 -0300

    Mais sobre variáveis

---
+++ variaveis/exercicio_04.php
@@ -0,0 +1,12 @@
+<?php
+
+    // $nome = (int)$_GET["a"];
+    //var_dump($nome);
+
+    // $ip = $_SERVER["REMOTE_ADDR"]; // Pegar IP do client
+
+    // $page = $_SERVER["SCRIPT_NAME"];
+
+    var_dump($_SERVER);
+    // echo $page;
+?>
