5385fc280fabbaab7775fc998b4158e11cf6dc6e
Lucas Ribeiro <sunlight.lucas2@gmail.com>
2022-05-09 18:19:25 -0300

    Testando Operadores

---
+++ operadores/aula_01.php
@@ -0,0 +1,36 @@
+<?php
+    /* Exemplo 1 */
+    // $nome = "Lucas";
+
+    // echo $nome . " mais alguma coisa <br/>";
+    // $nome .= " Ribeiro";
+
+    // echo $nome;
+
+    /* Exemplo 2 */
+    // $valorTotal = 0;
+    // $valorTotal += 100;
+    // $valorTotal += 25;
+    // $valorTotal *= .9;
+
+    // echo $valorTotal;
+
+
+    /* Exemplo 3 */
+    $a = 10;
+    $b = 2;
+
+    echo $a + $b;
+
+    echo "<br/>";
+
+    echo $a - $b;
+    
+    echo "<br/>";
+
+    echo $a * $b;
+    
+    echo "<br/>";
+
+    echo $a / $b;   
+?>
